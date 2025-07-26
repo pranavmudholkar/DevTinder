7e373f9c4df87f1ba5879711155a35547f2f2522
pranavmudholkar <pranavmudholkar455@gmail.com>
2025-07-26 10:05:00 +0530

    adding pagination on the API

--- src/routes/user.js
+++ src/routes/user.js
@@ -60,9 +60,14 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
 	}
 });
 
-userRouter.get('/user/feed', userAuth, async (req, res) => {
+userRouter.get('/feed', userAuth, async (req, res) => {
 	try {
 		const loggedInUser = req.user;
+
+		const page = parseInt(req.query.page) || 1;
+		const limit = parseInt(req.query.limit) || 10;
+		const skip = (page - 1) * limit;
+
 		const connectionRequests = connectionRequest
 			.find({
 				$or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -80,7 +85,10 @@ userRouter.get('/user/feed', userAuth, async (req, res) => {
 				{ _id: { $nin: Array.from(hideUsersFromFeed) } },
 				{ _id: { $ne: loggedInUser._id } },
 			],
-		}).select(USER_SAFEDATA);
+		})
+			.select(USER_SAFEDATA)
+			.skip(skip)
+			.limit(limit);
 
 		res.send(users);
 	} catch (err) {
